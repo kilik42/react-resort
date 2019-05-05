f33ba0853fa70fa40c22c4e132050d4b880f0627
marvin evins <maevins@cps.edu>
2019-05-05 08:36:32 -0500

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import {FaAlignRight} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 
 /**
  * ComponentName
  */
 export class Navbar extends Component { // eslint-disable-line react/prefer-stateless-function
+
+  state = {
+     isOpen: false
+  }
+  handleToggle= ()=>{
+    this.setState({isOpen:!this.state.isOpen})
+  }
   render() {
     return (
-      <div>
-           hello fro navbar
-      </div>
+    <nav className="navbar">
+    <div className="nav-center">
+          <div className="nav-header">
+            <Link to="/">
+              <img src={logo} alt="beach resort"/>
+
+            </Link>
+          </div>
+    </div>
+
+    </nav>
     );
   }
 }
