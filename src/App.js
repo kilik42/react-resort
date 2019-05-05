b9c057409f734b02fc787234084eb857fb43cb23
marvin evins <maevins@cps.edu>
2019-05-05 07:46:35 -0500

    switch cont

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ function App() {
       <Route  exact path = "/" component= {Home}/>
       <Route  exact path = "/rooms/" component= {Rooms}/>
       <Route exact path = "/rooms/:slug" component= {SingleRoom}/>
+
+      <Route component= {Error}/>
   </Switch>
 
 
