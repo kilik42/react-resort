58165ae4294917d59b67733916d9c3465c64edb6
marvin evins <maevins@cps.edu>
2019-05-05 07:50:17 -0500

    added navbar comp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,18 @@
-<r</rfc>
+import React, { Component } from 'react';
+
+/**
+ * ComponentName
+ */
+export class Navbar extends Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+           hello fro navbar
+      </div>
+    );
+  }
+}
+
+
+
+export default Navbar;
